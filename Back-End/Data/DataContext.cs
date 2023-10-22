aa64fbc337a468fe3e760ed29aa4b473b90b932a
Helena M Luz <helena1703h@gmail.com>
2023-10-21 22:45:10 -0300

    FEAT: -> Teste

--- Back-End/Data/DataContext.cs
+++ Back-End/Data/DataContext.cs
@@ -9,10 +9,39 @@ namespace Back_End.Data
         {
         }
         public DbSet<VeiculoModel> Veiculos { get; set; }
-        public DbSet<EmissaoModel> Emissoes { get; set; }   
-        public DbSet<ProprietarioModel> Proprietarios { get; set;}
+        public DbSet<EmissaoModel> Emissoes { get; set; }
+        public DbSet<ProprietarioModel> Proprietarios { get; set; }
         public DbSet<RuaModel> Ruas { get; set; }
         public DbSet<UsuarioModel> Usuarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<VeiculoModel>()
+                .HasOne(v => v.Proprietario)
+                .WithOne()
+                .HasForeignKey<VeiculoModel>(v => v.ProprietarioId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EmissaoModel>()
+                .HasOne(e => e.Veiculo)
+                .WithOne()
+                .HasForeignKey<EmissaoModel>(e => e.VeiculoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EmissaoModel>()
+                .HasOne(e => e.Rua)
+                .WithOne()
+                .HasForeignKey<EmissaoModel>(e => e.RuaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProprietarioModel>()
+                .HasOne(p => p.Usuario)
+                .WithOne()
+                .HasForeignKey<ProprietarioModel>(p => p.UsuarioId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
     }
 }
--- Back-End/Program.cs
+++ Back-End/Program.cs
@@ -1,13 +1,29 @@
+global using Back_End.Data;
+using Back_End.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IEmissaoService, EmissaoService>
+
 var app = builder.Build();
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
