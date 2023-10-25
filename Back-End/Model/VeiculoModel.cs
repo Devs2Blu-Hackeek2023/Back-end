d6e430b276ce9fe5e13b01d4f4e3a8512ff030da
Rob Caputo <rbcaputo@gmail.com>
2023-10-24 22:23:15 -0300

    criando o serviço de cameras

--- Back-End/Model/VeiculoModel.cs
+++ Back-End/Model/VeiculoModel.cs
@@ -1,18 +1,18 @@
 ﻿namespace Back_End.Model
 {
-    public class VeiculoModel
-    {
-        public int Id { get; set; }
-        public string  Placa { get; set; } = string.Empty;
-        public string Modelo { get; set; } = string.Empty;
-        public int Ano { get; set; }
-        public string Marca { get; set; } = string.Empty;
-        public char Categoria { get; set; }
-        public string Motor { get; set; } = string.Empty;
-        public string Combustivel { get; set; } = string.Empty;
-        public double KmL { get; set; }
-        public int ProprietarioId { get; set; }
-        public ProprietarioModel Proprietario { get; set; }
-        public List<string> Modificacoes { get; set; } = new List<string>();
-    }
+	public class VeiculoModel
+	{
+		public int Id { get; set; }
+		public string Placa { get; set; } = string.Empty;
+		public string Modelo { get; set; } = string.Empty;
+		public int Ano { get; set; }
+		public string Marca { get; set; } = string.Empty;
+		public string Categoria { get; set; } = string.Empty;
+		public string Motor { get; set; } = string.Empty;
+		public string Combustivel { get; set; } = string.Empty;
+		public double KmL { get; set; }
+		public int ProprietarioId { get; set; }
+		public ProprietarioModel Proprietario { get; set; }
+		public string Modificacoes { get; set; } = string.Empty;
+	}
 }
---
+++ Back-End/Services/Camera/CameraService.cs
@@ -0,0 +1,57 @@
+﻿using Back_End.Data;
+using Back_End.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_End.Services.Camera
+{
+	public class CameraService : ICameraService
+	{
+		private readonly DataContext _context;
+		private readonly Random _random;
+		private List<string> currentVeiculos = new List<string>();
+
+		public CameraService(DataContext context)
+		{
+			_context = context;
+			_random = new Random();
+		}
+
+		public async Task<VeiculoModel?> SortearVeiculoAsync()
+		{
+			int totalVeiculos = await _context.Veiculos.CountAsync();
+
+			if (totalVeiculos == 0) return null;
+			else
+			{
+				int randomIndex = _random.Next(0, totalVeiculos);
+				VeiculoModel? randomVeiculo = await _context.Veiculos.OrderBy(v => v.Id)
+					.Skip(randomIndex)
+					.Take(1)
+					.FirstOrDefaultAsync();
+
+				return randomVeiculo;
+			}
+		}
+
+		//public async Task<EmissaoModel?> IniciarEmissaoAsync(string CEP)
+		//{
+		//	EmissaoModel emissao = new EmissaoModel()
+		//	{
+		//		Veiculo = await SortearVeiculoAsync(),
+		//		Rua = await _context.Ruas.Where(r => r.CEP == CEP).FirstOrDefaultAsync(),
+		//		DataInicio = DateTime.Now
+		//	};
+
+		//	await _context.SaveChangesAsync();
+
+		//	return emissao;
+		//}
+
+		//public async Task<EmissaoModel?> FinalizarEmissaoAsync(EmissaoModel? emissao)
+		//{
+
+		//}
+
+		// public async CreateEmissao() {Chama Iniciar e finalizar}
+	}
+}
