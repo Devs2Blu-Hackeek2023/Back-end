19df55f2f56fb74133dc211167a506b5d3d3c274
Edna de Paula <ednadepaulaeap@gmail.com>
2023-10-24 21:56:29 -0300

    VEiculoDTO preenchido

--- Back-End/DTOs/VeiculoDTO.cs
+++ Back-End/DTOs/VeiculoDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace Back_End.DTOs
+﻿using Back_End.Model;
+
+namespace Back_End.DTOs
 {
     public class VeiculoDTO
     {
+        public string Placa { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public int Ano { get; set; }
+        public string Marca { get; set; } = string.Empty;
+        public char Categoria { get; set; }
+        public string Motor { get; set; } = string.Empty;
+        public string Combustivel { get; set; } = string.Empty;
+        public double KmL { get; set; }
+        public int ProprietarioId { get; set; }
+        public string Modificacoes { get; set; } = string.Empty;
     }
 }
