3a7e00c48a8495b9df483bc82d14c15fc1adea2f
Helena M Luz <helena1703h@gmail.com>
2023-10-25 22:22:49 -0300

    FEAT: -> ViaCep Api

---
+++ Back-End/DTOs/ViaCepDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Back_End.DTOs
+{
+    public class ViaCepDTO
+    {
+        public string CEP { get; set; } = string.Empty;
+        public string Rua { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string UF { get; set; } = string.Empty;
+    }
+}
---
+++ Back-End/Services/ViaCep/IViaCep.cs
@@ -0,0 +1,9 @@
+﻿using Back_End.DTOs;
+
+namespace Back_End.Services.ViaCep
+{
+    public interface IViaCep
+    {
+        Task<ViaCepDTO> _GetViaCep(string cep);
+    }
+}
