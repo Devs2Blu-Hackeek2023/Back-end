9029e6d5642ed7a223d6c1be8a9db97ffb3d1bfe
Edna de Paula <ednadepaulaeap@gmail.com>
2023-10-27 01:06:27 -0300

    VeiculoPostDTO Correção de todos os Services e Controller de Veiculos

---
+++ Back-End/DTOs/VeiculoPostDTO.cs
@@ -0,0 +1,19 @@
+﻿using Back_End.Model;
+
+namespace Back_End.DTOs
+{
+    public class VeiculoPostDTO
+    {
+        public string Placa { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public int Ano { get; set; }
+        public string Marca { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+        public string Motor { get; set; } = string.Empty;
+        public string Combustivel { get; set; } = string.Empty;
+        public double KmL { get; set; }
+        public string Modificacoes { get; set; } = string.Empty;
+        public int ProprietarioId { get; set; }
+        
+    }
+}
