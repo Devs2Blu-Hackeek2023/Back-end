edcebfc3760a157805b8b8d23af60ad451ffe601
Helena M Luz <helena1703h@gmail.com>
2023-10-29 20:50:36 -0300

    FEAT: Arrumado o Programan.cs

--- Back-End/Program.cs
+++ Back-End/Program.cs
@@ -5,6 +5,7 @@ using Back_End.Services.Interfaces;
 using Back_End.Services.Login;
 using Back_End.Services.Trafego;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +15,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration["Urls"] = $"http://0.0.0.0:{port}";*/
 
 
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 /*builder.Services.AddDbContext<DataContext>(options => 
 { 
